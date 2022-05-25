d5a257894fc6fbc698ff9afbbc1511671b797313
Diego Alejandro Ramírez Jaimes <diego6382@gmail.com>
2022-05-25 18:20:38 -0500

    hardcoding DB values

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 //este js tiene la configuración de la base de datos.
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
-dotenv.config({path:"./variables.env"});
+/*import dotenv from 'dotenv';
+dotenv.config({path:"./variables.env"});*/
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {//, 'root', { 'avambananeras' ''
-   host: process.env.BD_HOST,//process.env.BD_HOST,'127.0.0.1'
+const db = new Sequelize('heroku_97b3c49ca6bee4e','bb873a3227394b','1e8b5e0c',{
+    //process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {//, 'root', { 'avambananeras' ''
+   host: 'us-cdbr-east-05.cleardb.net',//process.env.BD_HOST,//process.env.BD_HOST,'127.0.0.1'
    //port: process.env.BD_PORT,//process.env.BD_PORT,'3306'
    dialect: 'mysql',
    define: {
